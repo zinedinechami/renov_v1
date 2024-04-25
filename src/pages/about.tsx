5e96fedfe3f98a71ba695d28c86f1ebb960a693c
Zinedine Chami <zinedine.chami@gmail.com>
2024-04-25 22:04:05 +0200

    created about section, integrated elements

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,19 +1,27 @@
+import img_ex from "../assets/taylor-flowe-30omXayKjQo-unsplash.jpg";
+import Image from "next/image";
+
 export default function About() {
   return (
     <>
-      <section className=" bg-zinc-900 text-white lg:px-48 px-4 pt-20 pb-20">
+      <section className=" bg-zinc-900 text-white lg:px-48 px-4 pt-20 pb-20 ">
         <h1 className="bebas-neue-regular text-7xl  text-zinc-50">
-          par inserer nom
+          par = inserer nom
         </h1>
-        <p className="w-6/12 pt-5">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate
-          rerum illo ducimus ex quos veniam explicabo doloribus beatae ut dolore
-          iusto aperiam ipsa animi nesciunt commodi minus, eligendi alias quis.
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid,
-          nobis accusantium suscipit deserunt quam nulla delectus, deleniti
-          similique maiores omnis beatae laborum animi odio a repellat quo
-          adipisci. Iure, debitis.
-        </p>
+        <div className="flex flex-row">
+          <div className="border rounded-full image-wrapper mr-28 mt-12">
+            <Image src={img_ex} width={300} height={300} alt="person" />
+          </div>
+          <p className="w-6/12 pt-5 mt-14 text-xl font-sans">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate
+            rerum illo ducimus ex quos veniam explicabo doloribus beatae ut
+            dolore iusto aperiam ipsa animi nesciunt commodi minus, eligendi
+            alias quis. Lorem ipsum dolor sit, amet consectetur adipisicing
+            elit. Aliquid, nobis accusantium suscipit deserunt quam nulla
+            delectus, deleniti similique maiores omnis beatae laborum animi odio
+            a repellat quo adipisci. Iure, debitis.
+          </p>
+        </div>
       </section>
     </>
   );
