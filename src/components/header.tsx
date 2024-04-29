91930dcb6044a823913b94990e549634d8d51099
Zinedine Chami <zinedine.chami@gmail.com>
2024-04-30 00:40:46 +0200

    updated header and landing

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,35 +5,31 @@ import Image from "next/image";
 export default function Header() {
   return (
     <>
-      <nav className=" sticky top-0 w-full bg-zinc-50  h-20  border z-10 ">
+      <nav className="  top-0 w-full bg-zinc-50  h-28   z-10 ">
         <a href="#home">
-          <h1 className="bebas-neue-regular text-center sm:text-4xl text-2xl pt-6 flex-col text-zinc-900 ">
-            RÉN
-            <span className="decoration-zinc-900 underline underline-offset-8 decoration-1">
-              OV
-            </span>{" "}
-            À 9
+          <h1 className="bebas-neue-regular pl-28 sm:text-4xl text-xl pt-10 flex-col text-zinc-900 ">
+            RÉNOV À 9
           </h1>
         </a>
-        <div className=" absolute top-6 left-20">
-          <ul className="flex ">
-            <li className="pr-4">
+        <div className=" absolute top-10 right-28">
+          <ul className="flex">
+            <li className="pt-2 pr-4">
               <a className="hover:opacity-60 transition-opacity" href="">
                 <Image src={insta} width={25} height={20} alt={"instagram"} />
               </a>
             </li>
-            <li>
+            <li className="pt-2 pr-6">
               <a className="hover:opacity-60 transition-opacity" href="">
                 <Image src={tiktok} width={25} height={20} alt={"tiktok"} />
               </a>
             </li>
+            <a href="#contact">
+              <button className="  flex bebas-neue-regular sm:text-xl bg-zinc-900 hover:bg-zinc-700  text-white  py-2 px-4 border border-zinc-200 rounded-lg shadow-md transition-colors">
+                Contactez Nous
+              </button>
+            </a>
           </ul>
         </div>
-        <a href="#contact">
-          <button className=" absolute right-16 top-4 bebas-neue-regular sm:text-xl bg-zinc-900 hover:bg-zinc-700  text-white  py-2 px-4 border border-zinc-200 rounded-lg shadow-md transition-colors">
-            Contactez Nous
-          </button>
-        </a>
       </nav>
     </>
   );
--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -5,24 +5,27 @@ export default function LandingPage() {
     <>
       <section
         id="home"
-        className=" h-full w-full  [background:radial-gradient(125%_125%_at_50%_10%,#fafafa_50%,#14b8a6_100%)] flex flex-col md:flex-row md:pt-16 pb-24 lg:px-48 px-4"
+        className=" h-full w-full  [background:radial-gradient(125%_125%_at_50%_10%,#fafafa_45%,#14b8a6_100%)] flex flex-col md:flex-row md:pt-6 pb-24 lg:px-44 px-4"
       >
-        <div className="sm:w-3/5 mx-auto  pt-14">
+        <div className="sm:w-3/5 mx-auto  pt-12">
           <h1 className="bebas-neue-regular text-7xl font-bold text-zinc-900">
             Rénovez votre bien <span className="text-emerald-400">à neuf</span>{" "}
           </h1>
-
-          <ul className="text-zinc-800 font-bold">
+          <p className="text-zinc-800">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid,
+            repellendus perferendis!
+          </p>
+          <ul className="text-zinc-800 font-bold pt-2">
             <li>Sur Paris, Ile de France</li>
             <li>Joiniable 7 jours sur 7 </li>
           </ul>
           <a href="#contact">
-            <button className="transition-colors bebas-neue-regular tracking-wider text-2xl bg-emerald-400 hover:bg-emerald-500 text-white my-6 py-2 px-8 border border-emerald-500 rounded-lg shadow-lg">
-              Obtenez un devis gratuit
+            <button className="transition-colors bebas-neue-regular text-bol tracking-wider text-2xl bg-emerald-400 hover:bg-emerald-500 text-white my-6 py-2  px-8 border border-emerald-500 rounded-xl shadow-lg">
+              Je veux un devis gratuit
             </button>
           </a>
         </div>
-        <div className="relative w-full  sm:pl-28">
+        <div className=" w-full  sm:pl-28">
           <Carousel />
         </div>
       </section>
