0a9ec8c6318db2f0e7a80858f7f8ac854dd7e6d9
Zinedine Chami <zinedine.chami@gmail.com>
2024-05-09 18:40:37 +0200

    added props to avis cards component

--- src/components/avis_card.tsx
+++ src/components/avis_card.tsx
@@ -10,13 +10,9 @@ export default function AvisCard({ title, text, name }: Props) {
   return (
     <>
       <article className="w-96 p-5 text-black bg-zinc-50 rounded-xl mr-4">
-        <h1 className="text-xl font-bold pb-2 ">Totalement Ravis</h1>
-        <p className="text-zinc-500 text-xs">
-          {" "}
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et suscipit
-          assumenda, temporibus non eligendi repellat recusandae aut?
-        </p>
-        <h1 className="font-bold pt-2">Nassera</h1>
+        <h1 className="text-xl font-bold pb-2 ">{title}</h1>
+        <p className="text-zinc-500 text-xs">{text}</p>
+        <h1 className="font-bold pt-2">{name}</h1>
       </article>
     </>
   );
--- src/pages/avis.tsx
+++ src/pages/avis.tsx
@@ -5,26 +5,62 @@ export default function Avis() {
     <>
       <section
         id="avis"
-        className="bg-zinc-950 text-white lg:pl-52 pr-32 px-4 pt-32  pb-40 bg-gradient-to-b from-zinc-900 to-black"
+        className="bg-zinc-950 text-white relative lg:pl-52 pr-32 px-4 pt-32  pb-40 bg-gradient-to-b from-zinc-900 to-black"
       >
         <h1 className="font-bold text-blue-500 pb-2 text-xs ">Temoiniages</h1>
         <h1 className=" bebas-neue-regular font-bold text-5xl  text-zinc-50 pb-12 ">
           Nos clients sont ravis
         </h1>
         <div className="flex p-6">
-          <AvisCard />
-          <AvisCard />
-          <AvisCard />
+          <AvisCard
+            title="Totalement Ravis"
+            text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et suscipit assumenda, temporibus non eligendi repellat recusandae aut?"
+            name="Nassera"
+          />
+          <AvisCard
+            title="Totalement Ravis"
+            text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et suscipit assumenda, temporibus non eligendi repellat recusandae aut?"
+            name="Nassera"
+          />
+          <AvisCard
+            title="Totalement Ravis"
+            text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et suscipit assumenda, temporibus non eligendi repellat recusandae aut?"
+            name="Nassera"
+          />
         </div>
         <div className="flex px-6 pb-6">
-          <AvisCard />
-          <AvisCard />
-          <AvisCard />
+          <AvisCard
+            title="Totalement Ravis"
+            text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et suscipit assumenda, temporibus non eligendi repellat recusandae aut?"
+            name="Nassera"
+          />
+          <AvisCard
+            title="Totalement Ravis"
+            text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et suscipit assumenda, temporibus non eligendi repellat recusandae aut?"
+            name="Nassera"
+          />
+          <AvisCard
+            title="Totalement Ravis"
+            text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et suscipit assumenda, temporibus non eligendi repellat recusandae aut?"
+            name="Nassera"
+          />
         </div>
-        <div className="flex px-6 ">
-          <AvisCard />
-          <AvisCard />
-          <AvisCard />
+        <div className="flex px-6  ">
+          <AvisCard
+            title="Totalement Ravis"
+            text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et suscipit assumenda, temporibus non eligendi repellat recusandae aut?"
+            name="Nassera"
+          />
+          <AvisCard
+            title="Totalement Ravis"
+            text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et suscipit assumenda, temporibus non eligendi repellat recusandae aut?"
+            name="Nassera"
+          />
+          <AvisCard
+            title="Totalement Ravis"
+            text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et suscipit assumenda, temporibus non eligendi repellat recusandae aut?"
+            name="Nassera"
+          />
         </div>
       </section>
     </>
